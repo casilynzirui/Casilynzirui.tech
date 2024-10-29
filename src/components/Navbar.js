da0d99e8ef6a196c8e45b129f6a6382f91319fb9
casilynzirui <casilynzirui@gmail.com>
2024-10-30 01:18:36 +0800

    Navbar basic implementation

---
+++ src/components/Navbar.js
@@ -0,0 +1,49 @@
+// src/Navbar.js
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+
+const Navbar = () => {
+  const [isHovered, setIsHovered] = useState(false); // Initialize state
+
+  return (
+    <nav className="navbar">
+      <div className="top-bar"></div>
+      <div className="header-bar">
+        <h1 className="logo">
+          <Link to="/" className="link">Casilynzirui</Link>
+        </h1>
+        <ul className="nav-links">
+          <li>
+            <Link to="/" className="link">Home</Link>
+          </li>
+          <li>
+            <Link to="/about" className="link">About</Link>
+          </li>
+          <li>
+            <Link to="/projects" className="link">Projects</Link>
+          </li>
+          <li>
+            <Link to="/contact" className="link">Contact</Link>
+          </li>
+          <li>
+            <div
+                className={`resume-link ${isHovered ? 'hovered' : ''}`}
+                onMouseEnter={() => setIsHovered(true)}
+                onMouseLeave={() => setIsHovered(false)}
+                onClick={() => {
+                  // Navigate to resume page, use window.location or a router if needed
+                  window.location.href = '/resume';
+                }}
+              >
+                Resume
+            </div>          
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+
+export default Navbar;
